3501882d0132e313f1858ed3995b45859ff3ed03
Oäk Kingkaew <oakabc@gmail.com>
2023-12-13 08:54:08 +0700

    Add files via upload

--- database.js
+++ database.js
@@ -1,5 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser")
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const mysql = require("mysql2/promise");
 const dbConn = mysql.createConnection({
@@ -24,6 +28,45 @@ app.get('/students/:id', async (req,res)=>{
     const rows = await connection.query('SELECT * from students where id = ' +req.params.id)
     res.send(rows)
 })
+
+
+app.delete("/students/:id", async (req, res) => {
+    const id = req.params.id;
+    const connection = await dbConn
+    await connection.query('DELETE FROM students WHERE id = ' + id)
+    res.status(204).send();
+})
+/*
+{
+    "name": "Oak",
+    "age": 26,
+    "phone": "0123456789",
+    "email": "oak@gmail.com"
+}
+*/
+app.post("/students", async (req, res) => {
+    const name = req.body.name;
+    const age = req.body.age;
+    const phone = req.body.phone;
+    const email = req.body.email;
+
+    const connection = await dbConn
+    const rows = await connection.query(`INSERT INTO students(name, age, phone, email) values ("${name}", ${age}, "${phone}", "${email}")`)
+    res.status(201).send({ result: "success" });
+})
+
+app.put("/students/:id", async (req, res) => {
+    const id = req.params.id;
+    const name = req.body.name;
+    const age = +req.body.age;
+    const phone = req.body.phone;
+    const email = req.body.email;
+
+    const connection = await dbConn
+    const rows = await connection.query(`UPDATE students SET name = "${name}", age = ${age}, phone = "${phone}", email = "${email}" WHERE id = ${id}`)
+    res.status(201).send({ result: "update success" });
+})
+
 
 
 app.get("/query-1", async (req, res) => {
